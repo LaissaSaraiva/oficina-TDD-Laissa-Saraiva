c1d0cc37a96e3c0d916df51cabab34f378c7237c
Laissa Cunha <laissa_saraiva_k2@whirlpool.com>
2025-05-10 13:03:54 -0300

    test: add class Calculadora + test

--- calculadora.test.js
+++ calculadora.test.js
@@ -1,21 +1,26 @@
 const Calculadora = require('./calculadora')
 
 describe('Calculadora', () => {
+  let calculadora;
+  beforeEach(() => {
+    calculadora = new Calculadora()
+  })
+
   it('Deverá retornar a soma de dois números', () => {
-    //Seu código aqui
-    
+    expect(calculadora.soma(2, 3)).toBe(5);
+
   })
 
   it('Deverá retornar a subtração de dois números', () => {
-    //Seu código aqui
-    
+    expect(calculadora.subtracao(10, 5)).toBe(5);
+
   })
 
   it('Deverá retornar a multiplicação entre dois números', () => {
-    //Seu código aqui
+    expect(calculadora.multiplicacao(5, 2)).toEqual(10)
   })
 
   it('Deverá retornar a divisao entre dois números', () => {
-    //Seu código aqui
+    expect(calculadora.divisao(8, 2)).toEqual(4)
   })
 })
